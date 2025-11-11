8af26b783bb8fbaa9b62090c07463e709380706d
Thiago Mazzi <thiagodev728@gmail.com>
2025-11-11 09:59:25 -0300

    Models

---
+++ EnercheckAPI/Models/Plano.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EnercheckAPI.Models
+{
+    public class Plano
+    {
+        public Guid PlanoId { get; set; }
+        [Required]
+        public string PlanoNome { get; set; }
+        public decimal? Valor { get; set; }
+        public bool? Ativo { get; set; }
+        public int? QuantidadeUsers { get; set; }
+    }
+}
---
+++ EnercheckAPI/Models/Projeto.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Nodes;
+
+namespace EnercheckAPI.Models
+{
+    public class Projeto
+    {
+        public Guid ProjetoId { get; set; }
+        public Guid UsuarioId { get; set; }
+        public IdentityUser? Usuario { get; set; }
+        [Required]
+        [Display(Name = "Nomde do Projetos")]
+        public string? Nome { get; set; }
+        public DateTime? DataCriacao { get; set; }
+        public int Progresso { get; set; }
+        [Display(Name = "Descrição do projeto")]
+        [DataType(DataType.MultilineText)]
+        public string? Descricao { get; set; }
+        public JsonArray? Analise { get; set; }
+    }
+}
